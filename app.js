6994ef6ff7665b474badda3127dcee27ab0f9dd5
michaelost <mikhaylo.ostroviy@gmail.com>
2017-04-11 18:07:03 +0300

    minor fix

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 import Event from './models/event';
 import moment from 'moment';
 import mongoose from 'mongoose';
-require('dotenv-extended').load();
 import restify from 'restify';
 import builder from 'botbuilder';
-
 import { bot } from './bot/bot.js';
 import server from './bot/server.js';
 import dialogs from './bot/dialogs';
-
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-  
-const url = 'mongodb://localhost:27017/skypebot';
