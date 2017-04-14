9c71d0ef86308fb22dd47461c5c3c891918bc46b
Max <maxym.fedas@gmail.com>
2017-04-14 13:32:32 +0300

    minor fix

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ var expect = require('chai').expect;
 var should = require('chai').should();
 var forTest = require('../testFile.js');
 
-describe('Number check', function() {
-	describe('return value', function() {
-		it('should return number for a given string', function() {
+describe('Number check', () => {
+	describe('return value', () => {
+		it('should return number for a given string', () => {
 			assert.equal(typeof forTest.convert('123'), 'number');
 		});
 	});
@@ -15,45 +15,41 @@ describe('Number check', function() {
 
 
 
-describe('Check EventSchema', function() {
-	describe('should exist', function() {
-		it('user should exist and be an object', function() {
+describe('Check EventSchema', () => {
+	describe('should exist', () => {
+		it('user should exist and be an object', () => {
 			should.exist(forTest.user);
 			forTest.user.should.be.an('object');
 		});
 	});
-	describe('user properties', function() {
-		it('should have startsAt and be a date', function() {
+
+	describe('check properties', () => {
+		it('should have startsAt and be a date', () => {
 			forTest.user.should.have.property('startsAt');
 			expect(forTest.user.startsAt).to.be.a('Date');
 		});
-	});
-	describe('user properties', function() {
-		it('should have endsAt and be a date', function() {
+
+		it('should have endsAt and be a date', () => {
 			forTest.user.should.have.property('endsAt');
 			expect(forTest.user.endsAt).to.be.a('Date');
 		});
-	});
-	describe('user properties', function() {
-		it('should have type and be a string', function() {
+
+		it('should have type and be a string', () => {
 			forTest.user.should.have.property('type');
 			expect(forTest.user.type).to.be.a('string');
 		});
-	});
-	describe('user properties', function() {
-		it('should have comment and be a string', function() {
+
+		it('should have comment and be a string', () => {
 			forTest.user.should.have.property('comment');
 			expect(forTest.user.comment).to.be.a('string');
 		});
-	});
-	describe('user properties', function() {
-		it('should have user and be a string', function() {
+
+		it('should have user and be a string', () => {
 			forTest.user.should.have.property('user');
 			expect(forTest.user.user).to.be.a('string');
 		});
-	});
-	describe('user properties', function() {
-		it('should have responses and be a array', function() {
+
+		it('should have responses and be a array', () => {
 			forTest.user.should.have.property('responses');
 			expect(forTest.user.responses).to.be.a('Array');
 		});
