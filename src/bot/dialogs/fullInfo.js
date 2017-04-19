821e85e214b1a397919e611319cb195bca258273
Max <maxym.fedas@gmail.com>
2017-04-19 18:36:23 +0300

    improve full info output

--- src/bot/dialogs/fullInfo.js
+++ src/bot/dialogs/fullInfo.js
@@ -4,17 +4,28 @@ import mongoose from 'mongoose';
 bot.dialog('/fullInfo', [
 	function(session) {
 		var usersDB = mongoose.connection.model('Users');
+		var holidays = mongoose.connection.model('Holidays');
+		var workedMonths = null;
+		holidays.findOne({year: '2017'}, (err, info) => {
+			workedMonths = info.months;
+		});
 		usersDB.findOne({name: session.userData.profile.name}, function(err, user) {
 			var answer = 'Sorry, not enough information. :(';
 			if (user) {
 				answer = 'Name: ' + user.name + '\n\n';
 				answer += 'Email: ' + user.email + '\n\n';
-				answer += 'Actualy days worked: ' + user.workedActually + '\n\n';
-				answer += 'Working days : ' + user.workingDays + '\n\n';
+				answer += 'Role: ' + user.role + '\n\n';
+				answer += 'sickLeaveLeft: ' + user.sickLeaveLeft + '\n\n';
+				answer += 'sickLeaveHalfLeft: ' + user.sickLeaveHalfLeft + '\n\n';
 				var vacationDays = 0;
-				for (var i = 0; i < 12; i++) {
-					vacationDays += (20 / 12) * (user.workedActually[i] / user.workingDays[i]);
-				}
+				user.workingInfo.map((year) => {
+					holidays.findOne({year: year.year}, (err, info) => {
+						workedMonths = info.months;
+					});
+					year.months.map((month, index) => {
+						vacationDays += (20 / 12) * (month.actuallyWorkedDays / workedMonths[index].totalWorkingDays);
+					});
+				});
 				vacationDays -= user.usedVacations;
 				answer += 'You have: ' + parseInt(vacationDays) + ' vacation days left';
 			}
