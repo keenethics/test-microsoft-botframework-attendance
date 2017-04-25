916e02e0b613182ed72d7be14becf9daeeeb038f
Max <maxym.fedas@gmail.com>
2017-04-25 17:03:23 +0300

    added notification user about rejected of his event

--- src/models/db/methods/usersMethods.js
+++ src/models/db/methods/usersMethods.js
@@ -60,7 +60,28 @@ function getRoleByUsername(username, callback) {
 }
 
 
+function setSessionAddressForUser(username, address) {
+	usersDB.update({name: username}, {$set: {sessionAddress: address}}, (err) => {
+		if(err) {
+			return console.log(err);
+		}
+	});
+}
+
+function getSessionAddressForUser(username, callback) {
+	usersDB.findOne({name: username}, (err, user) => {
+		if (err) {
+			return console.log(err);
+		}
+		if (user) {
+			callback(user.sessionAddress);
+		}
+	});
+}
+
 module.exports = {
 	getInfoByUsername: getInfoByUsername,
-	getRoleByUsername: getRoleByUsername
+	getRoleByUsername: getRoleByUsername,
+	setSessionAddressForUser: setSessionAddressForUser,
+	getSessionAddressForUser: getSessionAddressForUser
 };
