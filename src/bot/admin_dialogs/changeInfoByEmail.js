29cd2adedf29eb793d689e86c9cacd6f25b1e354
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-17 18:28:20 +0300

    Minor fix

--- src/bot/admin_dialogs/changeInfoByEmail.js
+++ src/bot/admin_dialogs/changeInfoByEmail.js
@@ -3,7 +3,7 @@ import builder from 'botbuilder';
 import mongoose from 'mongoose';
 const usersDB = mongoose.connection.model('Users');
 
-const user = {};
+const tempUserInfo = {};
 
 bot.dialog('/changeUserInfo', [
   function(session) {
@@ -15,21 +15,26 @@ bot.dialog('/changeUserInfo', [
     builder.Prompts.text(session, 'Enter user\'s email(example@keenethics.com) whose information you want to change');
   },
   function(session, result) {
-    user.email = result.response;
-    usersDB.findOne({email: 'taras.mazurkevych@keenethics.com'}, (err, user) => {
+    tempUserInfo.email = result.response;
+
+    usersDB.findOne({email: tempUserInfo.email}, (err, user) => {
       if (err) {
         console.error(err);
       }
 
+      tempUserInfo.newRole = user.role === 'admin' ? 'user' : 'admin';
+
       builder.Prompts.text(session,
-        `User\'s role is ${user.role}. Send '+' to change it to ${user.role === 'admin' ? 'user' : 'admin'}`);
+        `User\'s role is "${user.role}". Send "yes" to change it to "${tempUserInfo.newRole}".`);
     });
   },
   function(session, result) {
     let answer = result.response;
-    if (answer.indexOf('+')) {
-      usersDB.findOneAndUpdate({email: user.email},
-        {role: user.role === 'admin' ? 'user' : 'admin'}, () => {
+    if (answer.indexOf('yes') !== -1) {
+      usersDB.findOneAndUpdate(
+        {email: tempUserInfo.email},
+        {role: tempUserInfo.newRole},
+        () => {
           session.send('The role was changed.');
           session.endDialog();
         });
