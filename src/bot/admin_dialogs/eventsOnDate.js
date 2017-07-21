caa08eeacd57d5aaa075a2bf6e011759c203a9e7
Julien Bisconti <julienbi@unity3d.com>
2017-07-21 12:01:33 +0300

    minor fix

--- src/bot/admin_dialogs/eventsOnDate.js
+++ src/bot/admin_dialogs/eventsOnDate.js
@@ -8,14 +8,8 @@ import { getMomentDDMMFormat } from '../helpers/date.js';
 bot.dialog('/eventsOnDate', [
   async function(session, args) {
     session.send('welcome to events on dialog');
-    console.log(args.matched.input);
     const query = args.matched.input;
-    console.log("query ");
-    console.log(query)
-    console.log(query.split("events on "));
     const date = query.split('events on ').filter(d => (d))[0];
-    console.log("date12345");
-    console.log(date);
     const validDate = validateDate(date);
     let events = [];
     if (!validDate) {
@@ -24,8 +18,6 @@ bot.dialog('/eventsOnDate', [
     } else if (validDate == 2 || validDate == 1) {
       const [startDate, endDate] = date.split('-');
       events = await getEventsOnDate(getMomentDDMMFormat(startDate), getMomentDDMMFormat(endDate)); 
-      console.log('events');
-      console.log(events);
     } 
     session.endDialog(); 
   }
