816aacf91075e2ce09588c4d1ab79ba13f1183ff
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-25 13:00:02 +0300

    UX improvements and refactoring

--- src/bot/dialogs/help.js
+++ src/bot/dialogs/help.js
@@ -1,9 +1,9 @@
 import { bot } from '../bot.js';
 
-const userOptions = 'You can : 1. day off 2. edit profile 3.full info 5.vacation 6.active events';
+const userOptions = 'You can: - day off - edit profile - info on me - vacation - active events';
 
-const adminOptions = 'You can : 1. day off 2. editprofile 3.full info 4. user info 5.change info ' +
-  '6.vacation 7.active events 8. confirm events 9. settings 10. user status 11. info on';
+const adminOptions = 'You can: - day off - edit profile - info on me / email / Name - vacation - ' +
+  'active events - change info - confirm events - settings - What\'s email status on dd.mm.yyyy';
 
 bot.dialog('/help', [
   function(session) {
