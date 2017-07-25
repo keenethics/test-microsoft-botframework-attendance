76ca7375069a8671a7067ff8efe244b8cb5f6719
Julien Bisconti <julienbi@unity3d.com>
2017-07-25 19:03:02 +0300

    implement reject approve peding events from eventsOn dialog

--- src/bot/admin_dialogs/eventsOnDate.js
+++ src/bot/admin_dialogs/eventsOnDate.js
@@ -1,8 +1,11 @@
 import { bot } from '../bot.js';
 import { validateDate } from '../helpers/date.js';
-import { getEventsOnDate, getEventDate } from '../helpers/events.js'; 
+import { getEventsOnDate, getEventDate, approveOrRejectEvent } from '../helpers/events.js'; 
+import { getUserByEmail } from '../helpers/users.js'; 
 import { getMomentDDMMFormat } from '../helpers/date.js'; 
 import { ofEmail } from '../dialogs/dialogExpressions.js';
+import builder from 'botbuilder';
+
 bot.dialog('/eventsOnDate', [
   async function(session, args) {
     const statusExp = /(rejected |approved |pending )/;
@@ -18,14 +21,16 @@ bot.dialog('/eventsOnDate', [
       date = dateAndEmail[0];
       email = dateAndEmail[1];
     }
+    let events = [];
     const validDate = validateDate(date);
     if (!validDate) {
       session.send('incorect date...');
       session.endDialog();
     } else if (validDate == 2 || validDate == 1) {
       const [startDate, endDate] = date.split('-');
-      const events = (await getEventsOnDate(getMomentDDMMFormat(startDate), getMomentDDMMFormat(endDate), email, statusSpecified))
+      events = (await getEventsOnDate(getMomentDDMMFormat(startDate), getMomentDDMMFormat(endDate), email, statusSpecified))
         .sort((a,b) =>(a > b));
+      session.dialogData.mappedEvents = {};
       const displayEvents = events.map((ev,index) => (
         `${index} - ${getEventDate(ev)} ${ev.type} reason: ${ev.comment} user: ${ev.user}`
       ));
@@ -33,6 +38,38 @@ bot.dialog('/eventsOnDate', [
         session.send(ev);
       });
     } 
-    session.endDialog(); 
+    if (statusSpecified === 'pending') {
+      session.dialogData.statusSpecified = statusSpecified;
+      events.forEach((ev, index) => { session.dialogData.mappedEvents[index] = ev._id; });
+        builder.Prompts.text(session, 'you can approve or reject event by typing "approve/reject {n}'); 
+    } else {
+      session.endDialog();
+    }
+    session.send('type "menu" to go back');
+  },
+  async function(session, results) {
+    const { mappedEvents } = session.dialogData;
+    const action = results.response;
+    if (action == 'menu') session.beginDialog('/menu');
+    if ( session.dialogData.statusSpecified === 'pending') { 
+      const queryExp = /(reject |approve )*[0-9]{1,3}/;
+      if (!queryExp.test(action)) {
+        session.send("incorrect input");
+      } else {
+        const actionExp = /(reject |approve )/
+        const toDo = action.match(actionExp) && action.match(actionExp)[0].trim();
+        const index = parseInt(action.replace(actionExp, ''), 10);
+        const eventId = mappedEvents[index];
+        const user = await getUserByEmail(session.userData.profile.email);
+        const success = approveOrRejectEvent(eventId, user._id, `${toDo}d`);
+        if (success) {
+          session.send(`event ${mappedEvents[index]} has been ${toDo}d`);
+        } else {
+          session.send('ops... something wrong');
+        }
+      }
+    }
+    session.endDialog();
+    session.beginDialog('/menu');
   }
 ]);
