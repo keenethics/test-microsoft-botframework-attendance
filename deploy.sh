af5ab8058085019560fb50faa488d93cba0a4abb
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-26 15:37:25 +0300

    Minor fix

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git checkout -b $BRANCH
 npm run build
 git add .
 git commit -m "release"
-git push --force heroku master
+git push --force heroku $BRANCH:master
 git reset --hard HEAD^
-git branch -D $BRANCH:master
+git branch -D $BRANCH
 git checkout development
