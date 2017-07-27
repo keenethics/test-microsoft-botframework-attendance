32f25084f349990709377491d81e7d5dfd9a6593
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-27 11:56:56 +0300

    Fix

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,9 @@
 BRANCH=feature-`date +%y/%m/%d-%H.%M.%S`
 echo $BRANCH
 git checkout -b $BRANCH
-git status
 settings=$(<settings.json)
 git add .
 git add -f settings.json
-git status
 git commit -m "release"
 git push --force heroku $BRANCH:master
 git reset --hard HEAD^
