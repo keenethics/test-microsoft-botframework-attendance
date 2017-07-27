c6deb81ee11d9bb8b3474f496a744ece776a05ff
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-27 09:16:55 +0300

    Fixes in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 BRANCH=feature-`date +%y/%m/%d-%H.%M.%S`
 echo $BRANCH
 git checkout -b $BRANCH
-echo "node_modules/ npm-debug.log *.js.swp .idea/" >> t.gitignore
+echo "node_modules/ npm-debug.log *.js.swp .idea/" >> .gitignore
 npm run build
+git status
 git add .
+git status
 git commit -m "release"
 git push --force heroku $BRANCH:master
 git reset --hard HEAD^
