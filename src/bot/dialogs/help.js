58fdd767b5d56405ae5dd18fd061e50234b191c5
Taras Mazurkevych <mad.taras@gmail.com>
2017-07-27 14:41:17 +0300

    Fix typo

--- src/bot/dialogs/help.js
+++ src/bot/dialogs/help.js
@@ -3,7 +3,7 @@ import { bot } from '../bot.js';
 const userOptions = 'You can: \n- day off \n- edit profile \n- info on me \n- vacation \n' +
   '- active events';
 const adminOptions = 'You can: \n- day off \n- edit profile \n- info on me / email / Name \n' +
-  '- vacation \n-active events \n- change info \n- confirm events \n- settings \n- events on \n' +
+  '- vacation \n- active events \n- change info \n- confirm events \n- settings \n- events on \n' +
   '- What\'s email status on dd.mm.yyyy';
 
 bot.dialog('/help', [
