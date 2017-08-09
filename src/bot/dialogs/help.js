a1c3482c01fd4a516f0d23d0f421c4e18d804625
michaelost <julienbi@unity3d.com>
2017-08-09 17:17:02 +0300

    minor fix

--- src/bot/dialogs/help.js
+++ src/bot/dialogs/help.js
@@ -1,6 +1,6 @@
 import { bot } from '../bot.js';
 
-const userOptions = 'You can: \n- day off \n- edit profile \n- info on me \n- vacation \n' +
+const userOptions = 'Available options: \n- day off \n- edit profile \n- info on me \n- vacation \n' +
   '- active events \n' + 'i am admin \n';
 const adminOptions = 'You can: \n- day off \n- edit profile \n- info on me / email / Name \n' +
   '- vacation \n- active events \n- change info \n- confirm events \n- settings \n- events on \n' +
