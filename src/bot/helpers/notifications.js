65b3d5eabea85725859188a73860c6db7082d69f
michaelost <julienbi@unity3d.com>
2017-08-11 12:58:06 +0300

    log notification hepler

--- src/bot/helpers/notifications.js
+++ src/bot/helpers/notifications.js
@@ -5,6 +5,10 @@ import { getAdmins } from '../helpers/users.js';
 export const notifyAdmins = async (eventId) => {
   const ev = await getEvent(eventId); 
   const admins = await getAdmins(); 
+  console.log('admins');
+  console.log(admins);
+  console.log('ev');
+  console.log(ev);
   admins.forEach(ad => {
     const { address } = ad;
     bot.beginDialog(address, '/approveEvent', { event: ev });
