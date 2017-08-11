573cdf3f6e1f5c7c262bf5f6efd7abc12bfacc4a
michaelost <julienbi@unity3d.com>
2017-08-11 13:38:36 +0300

    admin recieve notification

--- src/bot/helpers/notifications.js
+++ src/bot/helpers/notifications.js
@@ -5,10 +5,6 @@ import { getAdmins } from '../helpers/users.js';
 export const notifyAdmins = async (eventId) => {
   const ev = await getEvent(eventId); 
   const admins = await getAdmins(); 
-  console.log('admins');
-  console.log(admins);
-  console.log('ev');
-  console.log(ev);
   admins.forEach(ad => {
     const { address } = ad;
     bot.beginDialog(address, '/approveEvent', { event: ev });
