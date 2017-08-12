afe9ec59129e03e4a4e306a4c5c30c99c511e183
michaelost <julienbi@unity3d.com>
2017-08-12 18:01:36 +0300

    rename active events to my upcoming events

--- src/bot/dialogs/help.js
+++ src/bot/dialogs/help.js
@@ -1,9 +1,9 @@
 import { bot } from '../bot.js';
 
 const userOptions = 'Available options: \n- day off \n- edit profile \n- info on me \n- vacation \n' +
-  '- active events \n' + 'i am admin \n';
+  '- my upcoming events \n' + 'i am admin \n';
 const adminOptions = 'You can: \n- day off \n- edit profile \n- info on me / email / Name \n' +
-  '- vacation \n- active events \n- change info \n- confirm events \n- settings \n- events on \n' +
+  '- vacation \n- my upcoming events \n- change info \n- confirm events \n- settings \n- events on \n' +
   '- add holiday on DD.MM.YYYY {name} \n' +
   '- holidays on MM.YYYY \n' +
   '- What\'s email status on dd.mm.yyyy \n' +
