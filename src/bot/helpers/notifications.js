b79d648e35c7028de146135b2ad070328b6da453
michaelost <julienbi@unity3d.com>
2017-08-12 17:57:57 +0300

    minor fix

--- src/bot/helpers/notifications.js
+++ src/bot/helpers/notifications.js
@@ -8,11 +8,10 @@ export const notifyAdmins = async (eventId) => {
   admins.forEach(ad => {
     const { address } = ad;
     const msg = new builder.Message().address(address);
-    const evText= `${getEventDate(ev)} ${ev.type} reason: ${ev.comment} user: ${ev.user} \n\n\n`;
+    const evText= `day off request: ${getEventDate(ev)} ${ev.type} reason: ${ev.comment} user: ${ev.user} \n\n\n`;
     msg.text(evText);
     msg.textLocale('en-US');
     bot.send(msg);
-    //bot.beginDialog(address, '/approveEvent', { event: ev });
   });
   return new Promise(function (resolve){ resolve('success'); });
 };
