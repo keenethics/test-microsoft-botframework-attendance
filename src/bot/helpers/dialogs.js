2bfa04738aa47cf58ba61acd60c35a52ffa8926f
michaelost <julienbi@unity3d.com>
2017-08-17 14:43:08 +0300

    fix quotes

--- src/bot/helpers/dialogs.js
+++ src/bot/helpers/dialogs.js
@@ -1,5 +1,4 @@
-export const filterQuotes = (str) => (str.replace(/(<)+(.)+?(>)+/,'').replace(/(<[/])+(.)+?(>)+/,''));
-
-export const addQuotes = (str, skipEnd) => (new RegExp(`^<.+>${str}<[/].+>${skipEnd ? '' : '$' }`, 'i'));
-
+export const filterQuotes = (str) => (str.replace(/(<.+?>)|(\[.+\].+:)|(\&lt;)|(\s){2,5}/g, ''));
+export const addQuotes = (str) => (new RegExp(`<.+?>[[].+].+: </.+?>((${str})([A-Za-z@.\\s])*)+<.+?>.+</.+?>`, 'i'));
 export const addQuotesToRegExp = (exp) => (addQuotes(`${exp}`.replace(/([/][^])+/, '').replace(/([$][/])+/,'')));
+
