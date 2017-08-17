ca9097329e65e4362ee6854ac61f4aae30c68e61
Taras Mazurkevych <mad.taras@gmail.com>
2017-08-17 13:28:33 +0300

    Fix typo

--- src/bot/dialogs/help.js
+++ src/bot/dialogs/help.js
@@ -1,10 +1,10 @@
 import { bot } from '../bot.js';
 
 const userOptions = 'You can: \n- day off \n- edit profile \n- info on me \n- vacation \n' +
-  '- active events \n- - day off [numberOfDays] dd.mm.yyyy [reason] \n- i am admin';
+  '- active events \n- day off [numberOfDays] dd.mm.yyyy [reason] \n- i am admin';
 const adminOptions = 'You can: \n- day off \n- edit profile \n- info on me / email / Name \n' +
-  '- vacation \n- my upcoming events \n- change info \n- requests waiting for my action \n- settings \n- events of {email} \n' +
-  '- add holiday on DD.MM.YYYY {name} \n' +
+  '- vacation \n- my upcoming events \n- change info \n- requests waiting for my action \n' +
+  '- settings \n- events of {email} \n- add holiday on DD.MM.YYYY {name} \n' +
   '- holidays on MM.YYYY \n' +
   '- What\'s email status on dd.mm.yyyy \n' +
   '- attendance of {email} on {mm.yyyy} \n' +
