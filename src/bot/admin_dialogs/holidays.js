a8fa377b86c439fa6396166830e96ddb9027c127
michaelost <julienbi@unity3d.com>
2017-08-19 16:56:52 +0300

    fix

--- src/bot/admin_dialogs/holidays.js
+++ src/bot/admin_dialogs/holidays.js
@@ -44,7 +44,11 @@ bot.dialog('/holidays', [
       session.beginDialog('/help');
       return;
     }
+    console.log(queryString);
+    console.log(queryString);
     const ind = parseInt(queryString.replace(/D+/g, ''));
+    console.log('ind');
+    console.log(ind);
     let op = null;
     const holidayId = session.dialogData.mathcIdInNumber[ind]; 
     op = await removeHoliday(holidayId);
